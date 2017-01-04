0b73345d2d93fb39a683890ed76fb6d6810722d7
goverdhan koyalkar <gkoyalk@searshc.com>
2017-01-04 09:53:05 +0000

    code clean up

--- js/services/details-service.js
+++ js/services/details-service.js
@@ -2,86 +2,10 @@
 
 app.factory("details", function($http, $q) {
     return {
-        getDetails: function(req) {
+        getDetails: function() {
             var deferred = $q.defer();
-
-            var details = [{
-                acctNumber: 876868876,
-                acctName: "Account 1",
-                currency: "DKK",
-                balInfo: [{
-                    date: "01/10/2016",
-                    desc: "ATM",
-                    amt: "4889"
-                }, {
-                    date: "02/10/2016",
-                    desc: "Interest",
-                    amt: "88"
-                }, {
-                    date: "04/10/2016",
-                    desc: "ATM",
-                    amt: "879"
-                }, {
-                    date: "04/10/2016",
-                    desc: "ATM",
-                    amt: "879"
-                }, {
-                    date: "04/10/2016",
-                    desc: "ATM",
-                    amt: "879"
-                }]
-            }, {
-                acctNumber: 098098876,
-                acctName: "Account 2",
-                currency: "DKK",
-                balInfo: [{
-                    date: "01/10/2016",
-                    desc: "ATM",
-                    amt: "4889"
-                }, {
-                    date: "02/10/2016",
-                    desc: "Interest",
-                    amt: "88"
-                }, {
-                    date: "04/10/2016",
-                    desc: "ATM",
-                    amt: "879"
-                }, {
-                    date: "04/10/2016",
-                    desc: "ATM",
-                    amt: "879"
-                }, {
-                    date: "04/10/2016",
-                    desc: "ATM",
-                    amt: "879"
-                }]
-            }, {
-                acctNumber: 76787687,
-                acctName: "Account 3",
-                currency: "DKK",
-                balInfo: [{
-                    date: "01/10/2016",
-                    desc: "ATM",
-                    amt: "4889"
-                }, {
-                    date: "02/10/2016",
-                    desc: "Interest",
-                    amt: "88"
-                }, {
-                    date: "04/10/2016",
-                    desc: "ATM",
-                    amt: "879"
-                }, {
-                    date: "04/10/2016",
-                    desc: "ATM",
-                    amt: "879"
-                }, {
-                    date: "04/10/2016",
-                    desc: "ATM",
-                    amt: "879"
-                }]
-            }, ];
-            deferred.resolve(details[req]);
+            var details = [];
+            deferred.resolve(details);
             return deferred.promise;
         }
     }
